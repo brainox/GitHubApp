ad9b6185ca082f9920c6aa50d0cde87c2fcf1126
obinna aguwa <macmartins081@gmail.com>
2022-08-30 23:50:08 +0100

    UICollectionView - FlowLayout

--- GitHubApp/Screens/FollowersListVC.swift
+++ GitHubApp/Screens/FollowersListVC.swift
@@ -30,9 +30,22 @@ class FollowersListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: width, height: width + 40)
+        
+        return UICollectionViewFlowLayout()
     }
     
     func getFollowers() {
